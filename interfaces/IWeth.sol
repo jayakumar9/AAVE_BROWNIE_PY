6bf4dd97d8d9b4aaba19a34cba46d46afc81777e
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-04 15:30:18 +0530

    saved at video 3.solidity time 1:25:34

---
+++ interfaces/IWeth.sol
@@ -0,0 +1,19 @@
+# create this file under AAVE_BROWNIE_PY/interfaces refer at video 3.solidity time 1:25:32 
+
+
+
+pragma solidity ^0.4.19;
+
+interface WethInterface {
+  function allowance(address owner, address spender) external view returns (uint256 remaining);
+  function approve(address spender, uint256 value) external returns (bool success);
+  function balanceOf(address owner) external view returns (uint256 balance);
+  function decimals() external view returns (uint8 decimalPlaces);
+  function name() external view returns (string memory tokenName);
+  function symbol() external view returns (string memory tokenSymbol);
+  function totalSupply() external view returns (uint256 totalTokensIssued);
+  function transfer(address to, uint256 value) external returns (bool success);
+  function transferFrom(address from, address to, uint256 value) external returns (bool success);
+  function deposit() external;
+  function withdraw(uint wad) external;
+}
