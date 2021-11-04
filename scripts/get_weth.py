0c4b5c6c66aa98f003f603cd3c59a1385a3f60d9
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-04 08:54:06 +0530

    saved at video 3.solidity time  1:24:57

---
+++ scripts/get_weth.py
@@ -0,0 +1,15 @@
+# create this file under AAVE_BROWNIE_PY/scripts refer at video 3.solidity time 1:23:52
+
+
+
+
+
+def main():
+    get_weth()
+    
+    
+def get_weth():
+    """
+    Mints WETH by depositing ETh.
+    
+    """
