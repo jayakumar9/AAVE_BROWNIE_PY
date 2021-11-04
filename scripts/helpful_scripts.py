9c163fa6cb88d8f24a869634183e013cfda80742
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-04 16:09:31 +0530

    saved at video 3.solidity time 1:29:32

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -2,16 +2,14 @@
 
 
 def get_account(index=None,id=None):
-    #accounts[0]
-    #accounts.add("env")
-    #accounts.load("id")
     if index:
         return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
     if id:
         return accounts.load(id)
-    if(
-        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
-        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
-      ):  
-        return accounts[0]
+    if network.show_active() in config["networks"]:
+        return accounts.add(config["wallets"]["from_key"])
+   return None
+   
     
