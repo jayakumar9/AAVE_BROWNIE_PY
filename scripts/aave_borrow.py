ea0ad39d9cf5f84fde4a051873c3b905fb2530b7
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-04 16:36:49 +0530

    saved at video 3.solidity time 1:36:02

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,1 +1,12 @@
 # create this file under AAVE_BROWNIE_PY/scripts
+
+from brownie import network, config
+from scripts.helpful_scripts import get_account
+from scripts.get_weth import get_weth
+
+def main():
+    account=get_account()
+    erc20_address=config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        get_weth()
+    
