7c04ccc4d3a08446bcc650625500bdfbea739181
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-04 16:03:33 +0530

    saved at video 3.solidity time 1:29:32

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -2,4 +2,16 @@
 
 
 def get_account(index=None,id=None):
+    #accounts[0]
+    #accounts.add("env")
+    #accounts.load("id")
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if(
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+      ):  
+        return accounts[0]
     
