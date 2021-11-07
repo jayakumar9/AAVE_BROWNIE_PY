5f845d21fd334f26df1f961f59ccf4dca10d130c
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-07 08:24:03 +0530

    saved at video 3.solidity time 2:01:30

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -19,7 +19,31 @@ def main():
     //print(lending_pool)
     #approve sending out ERC20 tokens
     approve_erc20(amount,lending_pool.address,erc20_address,account)
-   
+    print("Depositing...")
+    lending_pool.deposit(erc20_address,amount,account.address,0,{"from":account})
+    tx.wait(1)
+    print("Deposited!")
+    #....how much to borrow?
+    borrowable_eth,total_debt=get_borrowable_data(lending_pool,account)
+    
+def get_borrowable_data(lending_pool,account):   
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor,
+    )=lending_pool.getUserAccountData(account.address)   
+    available_borrow_eth= web3.fromwei(available_borrow_eth,"ether")
+    total_collateral_eth=web3.fromwei(total_collateral_eth,"ether")
+    total_debt_eth=web3.fromwei(total_debt_eth,"ether")
+    print(f"You have {total_collateral_eth} worth of ETH deposited.")
+    print(f"You have {total_debt_eth} worth of ETH borrowed.")
+    print(f"You can borrow {available_borrow_eth} worth of ETH .")
+    return(float(available_borrow_eth),float(total_debt_eth))
+
+
 
 def Approve_erc20(amount,spender,erc20_address,account):
     print("Approving ERC20 token...")
