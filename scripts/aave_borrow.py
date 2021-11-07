fd6e4e6d4b81ab948c5cea1c121f23e71570dfc6
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-07 10:10:08 +0530

    save

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -27,9 +27,12 @@ def main():
     borrowable_eth,total_debt=get_borrowable_data(lending_pool,account)
     print("Let's borrow!")
     # DAI in terms of ETH
-    dai_eth_price=get_asset_price(dai_eth_price_feed)
+    dai_eth_price=get_asset_price(config["networks"][network.show_active()]["dai_eth_price_feed"])
     
 def get_asset_price(price_feed_Address):
+    # ABI
+    # Address
+    dai_eth_price_feed=interface.AggregatorV3Interface
     
     
     
